27616d90910e38a583a247c5d8c8a458af13ebc8
Sandra Hall <71942530+sandi0805@users.noreply.github.com>
2021-01-09 09:37:52 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Local Postgres DB Info
+sf_username = 'sandee006xx@gmail.com'
+sf_password = 'Keira515*'
+sf_security_token = 'CZW98kiLN9DRJmJF0mkUYyMT'
+
+# Cloud/AWS MySQL DB Info
+remote_db_endpoint = 'codingbootcamp.cze9dquabwlh.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'root'
+remote_db_pwd = 'Dartlings!'
+remote_db_name = 'gwsis'
